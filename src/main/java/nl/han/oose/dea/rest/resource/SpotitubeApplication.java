b054d7ebd2aeec05ac3986c159fe008e6c77107c
jchen2011 <johnnychen2407@gmail.com>
2022-10-23 18:20:46 +0200

    Move SpotitubeApplication to rest map

--- src/main/java/nl/han/oose/dea/rest/resource/SpotitubeApplication.java
+++
@@ -1,7 +0,0 @@
-package nl.han.oose.dea.rest.resource;
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class SpotitubeApplication extends Application {
-}
