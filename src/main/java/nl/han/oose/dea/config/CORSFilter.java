44b4021c1673f8e24eb656d1c27f19eacad33cc1
jchen2011 <johnnychen2407@gmail.com>
2022-12-24 19:43:46 +0100

    fix(CORSFILTER): remove error

--- src/main/java/nl/han/oose/dea/config/CORSFilter.java
+++ src/main/java/nl/han/oose/dea/config/CORSFilter.java
@@ -9,7 +9,6 @@ import jakarta.ws.rs.ext.Provider;
 public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(final ContainerRequestContext requestContext,final ContainerResponseContext cres)  {
-        cres.getHeaders().add("Access-Control-Allow-Origin", "*");
         cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
         cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
